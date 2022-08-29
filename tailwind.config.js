0a771eb19bb6f9ff6dcd84658bef5a75f8be6111
Sidali <saadi.sidali@outlook.fr>
2022-08-29 20:19:37 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      darkText: '#3b4363',
+      scoreText: '#2a46c0',
+      headerOutline: '#606e85',
+      scissorsStart: '#ec9e0e',
+      scissorsEnd: '#eca922',
+      paperStart: '#4865f4',
+      paperEnd: '#5671f5',
+      rockStart: '#dc2e4e',
+      rockEnd: '#dd405d',
+      bgStart: '#1f3756',
+      bgEnd: '#141539',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
